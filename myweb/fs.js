61c5c8344c8c42eb3e408e5185877363d453cbb1
X 李 <cblzlz@outlook.com>
2019-09-04 01:22:26 +0800

    js test go on

--- myweb/fs.js
+++ myweb/fs.js
@@ -1,5 +1,14 @@
 var fs =require('fs');;
 
-var readme=fs.readFileSync("readme.txt","utf8");
+var readme=fs.readFileSync("readme.txt","utf8",function(err,data){
+    console.log(date);
+});
+
+fs.writeFileSync("writeme.txt",readme);
+fs.writeFileSync("writeme.txt","test again",function(err,data){
+    console.log(data);
+});
 
 console.log(readme);
+console.log(fs.readFileSync("writeme.txt","utf8"));
+console.log("我擦");
